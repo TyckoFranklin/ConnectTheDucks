a29d36293d895d2c25032ce4e6d2014a50549d48
Tycko Franklin <TheTyckoMan@gmail.com>
2019-01-10 22:09:36 -0800

    Initial commit

---
+++ callbackToPromise.js
@@ -0,0 +1,58 @@
+
+
+
+export const getTabAsync = function(options) {
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.tabs.query(options, resolve);
+        } catch (e) {
+            return reject(e);
+        }
+    });
+}
+
+// getTabAsync({ active: true })
+//     .then((a) => { console.log(a) })
+//     .catch((a) => { console.log("Error message in catch: ", a) });
+
+
+
+export const getTabAsyncProofWithPause =  function(options) {
+    return new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                chrome.tabs.query(options, resolve);
+            }, 5000);
+        } catch (e) {
+            return reject(e);
+        }
+    });
+}
+
+// getTabAsyncProofWithPause({ active: true })
+//     .then((a) => { console.log(a) })
+//     .catch((a) => { console.log("Error message in catch: ", a) });
+
+
+
+export const cTPResolveRejectArguments = function (callbackBasedFunction, nArguments) {
+    let [a, ...b] = arguments;
+    return new Promise((resolve, reject) => {
+        a(resolve, reject, ...b);
+    });
+}
+
+export const cTPArgumentResolve = function (callbackBasedFunction, options) {    
+    return new Promise((resolve, reject) => {
+        try {
+            callbackBasedFunction(options, resolve);
+        } catch (e) {
+            return reject(e);
+        }
+    });
+}
+export default {
+    getTabAsync,
+    cTPResolveRejectArguments,
+    cTPArgumentResolve
+}
